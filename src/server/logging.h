5b376d6a70a08a93de3a0eec12f2f59d6dca783e
Flungloaf <flungloaf@gmail.com>
2012-05-26 14:57:37 +0400

    Did some work -_-

---
+++ src/server/logging.h
@@ -0,0 +1,6 @@
+#ifndef H_SERVER_LOGGING_GUARD
+#define H_SERVER_LOGGING_GUARD
+
+void print_log(const char* thread_info, const char* msg, ...);
+
+#endif
---
+++ src/server/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define CONNECTION_REQUEST_QUEUE_BACKLOG_SIZE 10
+
+int server_socket;
+struct sockaddr_in server_addres;
+
+int main(int argc, char **args){
+	int client_socket, port, server, server_reuseaddr;
+	struct sockaddr_in client_addres;
+	socklen_t client_socklen;
+
+	if( argc != 2){
+		printf("Usage: %s port\n", args[0]);
+		printf("Creates a server listening at port\n");
+		exit(1);
+	}
+	
+	port = atoi(args[1]);
+	
+	if( (server_socket = socket(PF_INET, SOCK_STREAM, 0)) == -1 ){
+		perror("socket");
+		exit(1);
+	}
+	
+	server_reuseaddr = 1;
+	setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &server_reuseaddr, sizeof(server_reuseaddr));
+	
+	server_addres.sin_family = PF_INET;
+	server_addres.sin_port = htons(port);
+	server_addres.sin_addr.s_addr = htonl(INADDR_ANY);
+	client_socklen = sizeof(struct sockaddr_in);
+	
+	if( bind(server_socket, (struct sockaddr *)(&server_addres), sizeof(struct sockaddr_in)) == -1 ){
+		perror("bind");
+		exit(1);
+	}
+	
+	listen(server_socket, CONNECTION_REQUEST_QUEUE_BACKLOG_SIZE);
+	print_log("main", "Waiting for connections on %d port", port);
+	
+	while(1){
+		if( (client_socket = accept(server_socket, (struct sockaddr *)(&client_addres), &client_socklen)) == -1){
+			perror("accept");
+			exit(1);
+		}
+		print_log("main", "Client connected on socket %d", client_socket);
+	}
+	return 0;
+}
